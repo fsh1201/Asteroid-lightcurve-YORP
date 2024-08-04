66518f2e36a1f8295817b4fc147cb8515944b3af
fsh1201 <54531652+fsh1201@users.noreply.github.com>
2024-08-05 00:47:33 +0800

    update yorp.cpp and upload in
    The YORP detection is changed to use the configure file, instead of the requirement of changing parameters in the .cpp and remaking.

--- yorp.cpp
+++ yorp.cpp
@@ -1,9 +1,12 @@
 /* 
-	This program takes the light curve path and initial parameters as the input and 
+	This program takes the light curve and the range of initial parameters as the input and 
 	output the results of out areas, lightcurves, parameters and related chi2. 
-	This program works with 40 processes by default, and each process calculate 250
-	different initial random parameters in the range of the intervals set in lines 42-50 
-	int this yorp.cpp file, while the numbers 40 and 250 can be changed in lines 131 and 133.
+	This program works with 40 processes by default, and calculate with totally 1000
+	random initial parameters.
+	
+	The range of parameters can be pass through input_parameter file.
+	If the input_parameter file does not exsit or cannot be open, then this program will adopt
+	the default parameters already set in this yorp.cpp file.
 
 	Syntax:
 		yorp lc input_parameter out_area out_par out_lcs yorp_chi2
@@ -45,21 +48,31 @@ const double PI = 3.14159265358979323846;
 unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 default_random_engine gen(seed);
 
-uniform_real_distribution<double> dislambda(17.23122838 - 0.001, 17.23122838 + 0.001);
-uniform_real_distribution<double> disbeta(11.35821331 - 0.001, 11.35821331 + 0.001);
-uniform_real_distribution<double> disp(360.0 / (1639.38928 + 0.00001) * 24.0, 360.0 / (1639.38928 - 0.00001) * 24.0);
-uniform_real_distribution<double> disnu(-1.25e-9, 0.25e-9);
-uniform_real_distribution<double> disfa(0.039, 0.041);
-uniform_real_distribution<double> disfd(8.4 * PI / 180.0, 8.6 * PI / 180.0);
-uniform_real_distribution<double> disfk(-4.8e-4 * 180.0 / PI, -4.6e-4 * 180.0 / PI);
-uniform_real_distribution<double> disfc(0.11, 0.15);
-uniform_real_distribution<double> disfb(0.049, 0.051);
+/*default parameters*/
+uniform_real_distribution<double> dislambda(0., 360.);
+uniform_real_distribution<double> disbeta(-90., 90.);
+uniform_real_distribution<double> disp(2., 10.);
+uniform_real_distribution<double> disnu(-1e-7, 1e-7);
+uniform_real_distribution<double> disfa(0.009, 0.048);
+uniform_real_distribution<double> disfd(3.3 * PI / 180.0, 9.45 * PI / 180.0);
+uniform_real_distribution<double> disfk(-7.7e-4 * 180.0 / PI, -2.2e-4 * 180.0 / PI);
+uniform_real_distribution<double> disfc(0.05, 0.5);
+uniform_real_distribution<double> disfb(0.0142, 0.087);
+double JD0 = 0;
+double phi0 = 0;
+double con_reg = 0.1;
+int lm_l = 6, lm_m = 6;
+int rows = 8;
+double stopcondition = 100;
+int fflambda = 1, ffbeta = 1, ffp = 1, ffnu = 1, ffa = 0, ffd = 0, ffk = 0, ffb = 0, ffc = 0;	//fixed (0) or free (1)
+int ntotal = 1000, nth = 40;
 
 vector<string> yorp(string lcFileName, string inputFileName, string areaFileName, string parFileName, string outlcFileName,
-	vector<double> vlambda, vector<double> vbeta, vector<double> vP, vector<double> vnu)
+	vector<double> vlambda, vector<double> vbeta, vector<double> vP, vector<double> vnu, int nt)
 {
 	vector<string> vs;
-	for (int i = 0; i < vlambda.size(); i++)
+	int n = vlambda.size();
+	for (int i = 0; i < n; i++)
 	{
 		double lambda = vlambda[i];
 		double beta = vbeta[i];
@@ -68,36 +81,36 @@ vector<string> yorp(string lcFileName, string inputFileName, string areaFileName
 		char ch;
 		string line;
 		stringstream ssInputFileName("");
-		ssInputFileName << setprecision(10) << inputFileName << "_" << lambda << "_" << beta << "_" << P << "_" << nu << ".txt";
+		ssInputFileName << setprecision(10) << inputFileName << "_" << nt << "_" << i << ".txt";
 		fstream testfile(ssInputFileName.str(), ios::in);
 		testfile >> ch;
 		if ((!testfile.is_open()) || testfile.eof())
 		{
 			fstream outInFile(ssInputFileName.str(), ios::out);
 			outInFile << setprecision(15);
-			outInFile << lambda << "\t0\tinital lambda [deg] (0/1 - fixed/free)" << endl;
-			outInFile << beta << "\t0\tinitial beta [deg] (0/1 - fixed/free)" << endl;
-			outInFile << P << "\t0\tinital period [hours] (0/1 - fixed/free)" << endl;
-			outInFile << nu << "\t1\tinital YORP strength [rad/day^2] (0/1 - fixed/free)" << endl;
-			outInFile << "2451545.0\tzero time [JD]" << endl;
-			outInFile << "32.64\tinitial rotation angle [deg]" << endl;
-			outInFile << "0.1\tconvexity regularization" << endl;
-			outInFile << "6 6\tdegree and order of spherical harmonics expansion" << endl;
-			outInFile << "8\tnumber of rows" << endl;
-			outInFile << disfa(gen) << "\t0\tphase funct. param. 'a' (0/1 - fixed/free) " << endl;
-			outInFile << disfd(gen) << "\t0\tphase funct.param. 'd' (0/1 - fixed/free)" << endl;
-			outInFile << disfk(gen) << "\t0\tphase funct. param. 'k' (0/1 - fixed/free) " << endl;
-			outInFile << disfb(gen) << "\t0\tphase funct. param. 'b' (0/1 - fixed/free) " << endl;
-			outInFile << disfc(gen) << "\t0\tLambert coefficient 'c' (0/1 - fixed/free)" << endl;
+			outInFile << lambda << "\t" << fflambda << "\tinital lambda[deg](0 / 1 - fixed / free)" << endl;
+			outInFile << beta << "\t" << ffbeta << "\tinitial beta [deg] (0/1 - fixed/free)" << endl;
+			outInFile << P << "\t" << ffp << "\tinital period [hours] (0/1 - fixed/free)" << endl;
+			outInFile << nu << "\t" << ffnu << "\tinital YORP strength [rad/day^2] (0/1 - fixed/free)" << endl;
+			outInFile << JD0 << "\tzero time [JD]" << endl;
+			outInFile << phi0 << "\tinitial rotation angle [deg]" << endl;
+			outInFile << con_reg << "\tconvexity regularization" << endl;
+			outInFile << lm_l << " " << lm_m << "\tdegree and order of spherical harmonics expansion" << endl;
+			outInFile << rows << "\tnumber of rows" << endl;
+			outInFile << disfa(gen) << "\t" << ffa << "\tphase funct. param. 'a' (0/1 - fixed/free) " << endl;
+			outInFile << disfd(gen) << "\t" << ffd << "\tphase funct.param. 'd' (0/1 - fixed/free)" << endl;
+			outInFile << disfk(gen) << "\t" << ffk << "\tphase funct. param. 'k' (0/1 - fixed/free) " << endl;
+			outInFile << disfb(gen) << "\t" << ffb << "\tphase funct. param. 'b' (0/1 - fixed/free) " << endl;
+			outInFile << disfc(gen) << "\t" << ffc << "\tLambert coefficient 'c' (0/1 - fixed/free)" << endl;
 			outInFile << "100\titeration stop condition" << endl;
 			outInFile.close();
 		}
 		stringstream ssAreaFileName("");
-		ssAreaFileName << setprecision(10) << areaFileName << "_" << lambda << "_" << beta << "_" << P << "_" << nu << ".txt";
+		ssAreaFileName << setprecision(10) << areaFileName << "_" << nt << "_" << i << ".txt";
 		stringstream ssParFileName("");
-		ssParFileName << setprecision(10) << parFileName << "_" << lambda << "_" << beta << "_" << P << "_" << nu << ".txt";
+		ssParFileName << setprecision(10) << parFileName << "_" << nt << "_" << i << ".txt";
 		stringstream ssOutLcFileName("");
-		ssOutLcFileName << setprecision(10) << outlcFileName << "_" << lambda << "_" << beta << "_" << P << "_" << nu << ".txt";
+		ssOutLcFileName << setprecision(10) << outlcFileName << "_" << nt << "_" << i << ".txt";
 		fstream areaFile(ssAreaFileName.str(), ios::in); areaFile >> ch;
 		fstream parFile(ssParFileName.str(), ios::in); parFile >> ch;
 		fstream outlcFile(ssOutLcFileName.str(), ios::in);	outlcFile >> ch;
@@ -119,6 +132,7 @@ vector<string> yorp(string lcFileName, string inputFileName, string areaFileName
 
 int main(int argc, char** argv)
 {
+	
 	if (argc < 2)
 	{
 		cout << "yorp lc input_parameter out_area out_par out_lcs yorp_chi2" << endl;
@@ -130,20 +144,62 @@ int main(int argc, char** argv)
 	string parFileName(argv[4]);
 	string outlcFileName(argv[5]);
 	string yorp_chi2FileName(argv[6]);
+
+	ifstream infile(inputFileName, ios::in);
+	if (infile)
+	{
+		double low, high;
+		string line;
+		infile >> low >> high >> fflambda;	getline(infile, line);	//lambda
+		dislambda = uniform_real_distribution<double>(low, high);
+		infile >> low >> high >> ffbeta;	getline(infile, line);	//beta
+		disbeta = uniform_real_distribution<double>(low, high);
+		infile >> low >> high >> ffp;		getline(infile, line);	//p
+		disp = uniform_real_distribution<double>(low, high);
+		infile >> low >> high >> ffnu;		getline(infile, line);	//yorp
+		disnu = uniform_real_distribution<double>(low, high);
+		infile >> JD0;						getline(infile, line);	//jd0
+		infile >> phi0;						getline(infile, line);	//phi0
+		infile >> con_reg;					getline(infile, line);	//convexity regularization
+		infile >> lm_l >> lm_m;				getline(infile, line);	//degree and order of spherical harmonics expansion
+		infile >> rows;						getline(infile, line);	//number of rows
+		infile >> low >> high >> ffa;		getline(infile, line);	//phase funct. param. 'a'
+		disfa = uniform_real_distribution<double>(low, high);
+		infile >> low >> high >> ffd;		getline(infile, line);	//phase funct. param. 'd'
+		disfd = uniform_real_distribution<double>(low, high);
+		infile >> low >> high >> ffk;		getline(infile, line);	//phase funct. param. 'k'
+		disfk = uniform_real_distribution<double>(low, high);
+		infile >> low >> high >> ffb;		getline(infile, line);	//phase funct. param. 'b'
+		disfb = uniform_real_distribution<double>(low, high);
+		infile >> low >> high >> ffc;		getline(infile, line);	//phase funct. param. 'c'
+		disfc = uniform_real_distribution<double>(low, high);
+		infile >> stopcondition;			getline(infile, line);	//iteration stop condition
+		infile >> ntotal;					getline(infile, line);	//number of total random parameter sets
+		infile >> nth;						getline(infile, line);	//number of threads
+	}
+	else
+	{
+		cout << "failed to open the config file" << endl;
+		cout << "using the default parameters" << endl;
+	}
+
 	fstream yorpFile(yorp_chi2FileName, ios::out);
 	yorpFile << "# lambda(deg) beta(deg) p(hour) yorp(rad/d^2) rchisq jd0(JD) phi0(deg) a d k b c" << endl;
+
+	int n = ntotal / nth;
+
 	vector<future<vector<string>>> vfvs;
 	vector<double> vlambda, vbeta, vP, vnu;
-	for (int i = 0; i < 40; i++)
+	for (int i = 0; i < nth; i++)
 	{
-		for (int th = 0; th < 250; th++)
+		for (int th = 0; th < n; th++)
 		{
 			vlambda.push_back(dislambda(gen));
 			vbeta.push_back(disbeta(gen));
 			vP.push_back(disp(gen));
 			vnu.push_back(disnu(gen));
 		}
-		vfvs.push_back(async(yorp, lcFileName, inputFileName, areaFileName, parFileName, outlcFileName, vlambda, vbeta, vP, vnu));
+		vfvs.push_back(async(yorp, lcFileName, inputFileName, areaFileName, parFileName, outlcFileName, vlambda, vbeta, vP, vnu, i));
 		vlambda.clear();
 		vbeta.clear();
 		vP.clear();
