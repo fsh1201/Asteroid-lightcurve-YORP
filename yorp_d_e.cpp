b38b4ecc74f0ac07d44a360abc5097168a3a6808
fsh1201 <54531652+fsh1201@users.noreply.github.com>
2025-06-17 16:11:09 +0800

    Add files via upload
    update yorp_d_e, which can do both bootstrap at once now

--- yorp_d_e.cpp
+++ yorp_d_e.cpp
@@ -37,15 +37,30 @@ int main(int argc, char** argv)
 	cout << cmd << endl;
 	system(cmd.c_str());
 
-	cmd = "rm -rf " + path + "/inverse " + path + "/bootstrap";
-	cout << cmd << endl;
-	system(cmd.c_str());
+	if (dplc == 0 || dplc == 1)
+	{
+		cmd = "rm -rf " + path + "/inverse " + path + "/bootstrap" + to_string(dplc);
+		cout << cmd << endl;
+		system(cmd.c_str());
 
-	cmd = "mkdir " + path + "/inverse " + path + "/bootstrap " + path + "/inverse/in " + path + "/inverse/out "
-		+ path + "/bootstrap/in " + path + "/bootstrap/out " + path + "/bootstrap/lcbs "
-		+ path + "/inverse/out/best ";
-	cout << cmd << endl;
-	system(cmd.c_str());
+		cmd = "mkdir " + path + "/inverse " + path + "/bootstrap" + to_string(dplc) + " " + path + "/inverse/in " + path + "/inverse/out "
+			+ path + "/bootstrap" + to_string(dplc) + "/in " + path + "/bootstrap" + to_string(dplc) + "/out "
+			+ path + "/bootstrap" + to_string(dplc) + "/lcbs " + path + "/inverse/out/best ";
+		cout << cmd << endl;
+		system(cmd.c_str());
+	}
+	if (dplc == 2)
+	{
+		cmd = "rm -rf " + path + "/inverse " + path + "/bootstrap0 " + path + "/bootstrap1";
+		cout << cmd << endl;
+		system(cmd.c_str());
+
+		cmd = "mkdir " + path + "/inverse " + path + "/bootstrap0 " + path + "/bootstrap1 " + path + "/inverse/in " + path + "/inverse/out "
+			+ path + "/bootstrap0/in " + path + "/bootstrap0/out " + path + "/bootstrap1/in " + path + "/bootstrap1/out "
+			+ path + "/bootstrap0/lcbs " + path + "/bootstrap1/lcbs " + path + "/inverse/out/best ";
+		cout << cmd << endl;
+		system(cmd.c_str());
+	}
 
 	cmd = "cp " + parpath + " " + path + "/inverse/in/in";
 	cout << cmd << endl;
@@ -56,24 +71,91 @@ int main(int argc, char** argv)
 	cout << cmd << endl;
 	system(cmd.c_str());
 
-	cmd = "python ./ychi2bsin.py " + path + "/inverse/in/in " + path + "/inverse/out/yorp_chi2.txt.sort.txt "
-		+ path + "/bootstrap/in/in";
-	cout << cmd << endl;
-	system(cmd.c_str());
+	if (dplc == 0)
+	{
+		cmd = "python ./ychi2bsin.py " + path + "/inverse/in/in " + path + "/inverse/out/yorp_chi2.txt.sort.txt "
+			+ path + "/bootstrap0/in/in";
+		cout << cmd << endl;
+		system(cmd.c_str());
 
-	cmd = "cp " + lcpath + " " + path + "/bootstrap/lcbs/lc.txt";
-	cout << cmd << endl;
-	system(cmd.c_str());
+		cmd = "cp " + lcpath + " " + path + "/bootstrap0/lcbs/lc.txt";
+		cout << cmd << endl;
+		system(cmd.c_str());
 
-	cmd = "./yorp_e " + path + "/bootstrap/lcbs/lc.txt" + " " + path + "/bootstrap/in/in " + path + "/bootstrap/out/out_area "
-		+ path + "/bootstrap/out/out_par " + path + "/bootstrap/out/out_lcs " + path + "/bootstrap/out/yorp_chi2.txt "
-		+ to_string(nbs) + " " + to_string(nth) + " " + to_string(dplc);
-	cout << cmd << endl;
-	system(cmd.c_str());
+		cmd = "./yorp_e " + path + "/bootstrap0/lcbs/lc.txt" + " " + path + "/bootstrap0/in/in " + path + "/bootstrap0/out/out_area "
+			+ path + "/bootstrap0/out/out_par " + path + "/bootstrap0/out/out_lcs " + path + "/bootstrap0/out/yorp_chi2.txt "
+			+ to_string(nbs) + " " + to_string(nth) + " " + to_string(dplc);
+		cout << cmd << endl;
+		system(cmd.c_str());
 
-	cmd = "python ./fig1st.py " + path + "/bootstrap/results " + path + "/bootstrap/out/yorp_chi2.txt";
-	cout << cmd << endl;
-	system(cmd.c_str());
+		cmd = "python ./fig1st.py " + path + "/bootstrap0/results " + path + "/bootstrap0/out/yorp_chi2.txt";
+		cout << cmd << endl;
+		system(cmd.c_str());
+	}
+
+	if (dplc == 1)
+	{
+		cmd = "python ./ychi2bsin.py " + path + "/inverse/in/in " + path + "/inverse/out/yorp_chi2.txt.sort.txt "
+			+ path + "/bootstrap1/in/in";
+		cout << cmd << endl;
+		system(cmd.c_str());
+
+		cmd = "cp " + lcpath + " " + path + "/bootstrap1/lcbs/lc.txt";
+		cout << cmd << endl;
+		system(cmd.c_str());
+
+		cmd = "./yorp_e " + path + "/bootstrap1/lcbs/lc.txt" + " " + path + "/bootstrap1/in/in " + path + "/bootstrap1/out/out_area "
+			+ path + "/bootstrap1/out/out_par " + path + "/bootstrap1/out/out_lcs " + path + "/bootstrap1/out/yorp_chi2.txt "
+			+ to_string(nbs) + " " + to_string(nth) + " " + to_string(dplc);
+		cout << cmd << endl;
+		system(cmd.c_str());
+
+		cmd = "python ./fig1st.py " + path + "/bootstrap1/results " + path + "/bootstrap1/out/yorp_chi2.txt";
+		cout << cmd << endl;
+		system(cmd.c_str());
+	}
+
+	if (dplc == 2)
+	{
+		cmd = "python ./ychi2bsin.py " + path + "/inverse/in/in " + path + "/inverse/out/yorp_chi2.txt.sort.txt "
+			+ path + "/bootstrap0/in/in";
+		cout << cmd << endl;
+		system(cmd.c_str());
+
+		cmd = "cp " + lcpath + " " + path + "/bootstrap0/lcbs/lc.txt";
+		cout << cmd << endl;
+		system(cmd.c_str());
+
+		cmd = "./yorp_e " + path + "/bootstrap0/lcbs/lc.txt" + " " + path + "/bootstrap0/in/in " + path + "/bootstrap0/out/out_area "
+			+ path + "/bootstrap0/out/out_par " + path + "/bootstrap0/out/out_lcs " + path + "/bootstrap0/out/yorp_chi2.txt "
+			+ to_string(nbs) + " " + to_string(nth) + " 0";
+		cout << cmd << endl;
+		system(cmd.c_str());
+
+		cmd = "python ./fig1st.py " + path + "/bootstrap0/results " + path + "/bootstrap0/out/yorp_chi2.txt";
+		cout << cmd << endl;
+		system(cmd.c_str());
+
+
+		cmd = "python ./ychi2bsin.py " + path + "/inverse/in/in " + path + "/inverse/out/yorp_chi2.txt.sort.txt "
+			+ path + "/bootstrap1/in/in";
+		cout << cmd << endl;
+		system(cmd.c_str());
+
+		cmd = "cp " + lcpath + " " + path + "/bootstrap1/lcbs/lc.txt";
+		cout << cmd << endl;
+		system(cmd.c_str());
+
+		cmd = "./yorp_e " + path + "/bootstrap1/lcbs/lc.txt" + " " + path + "/bootstrap1/in/in " + path + "/bootstrap1/out/out_area "
+			+ path + "/bootstrap1/out/out_par " + path + "/bootstrap1/out/out_lcs " + path + "/bootstrap1/out/yorp_chi2.txt "
+			+ to_string(nbs) + " " + to_string(nth) + " 1";
+		cout << cmd << endl;
+		system(cmd.c_str());
+
+		cmd = "python ./fig1st.py " + path + "/bootstrap1/results " + path + "/bootstrap1/out/yorp_chi2.txt";
+		cout << cmd << endl;
+		system(cmd.c_str());
+	}
 
 	ifstream result(path + "/inverse/out/yorp_chi2.txt.sort.txt", ios::in);
 	getline(result, line);
